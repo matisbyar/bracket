b80338191ab77807fa8b79dc3042caab9fb070fe
Killian Reiner <killian.reiner@etu.umontpellier.fr>
2022-10-26 13:13:35 +0200

    add ajout getter/setter Commande.php

--- src/Model/DataObject/Commande.php
+++ src/Model/DataObject/Commande.php
@@ -3,5 +3,69 @@
 namespace App\Bracket\Model\DataObject;
 
 class Commande extends AbstractDataObject {
+    private int $id;
+    private string $adresse, $client;
+
+    /**
+     * @param int $id
+     * @param string $adresse
+     * @param string $client
+     */
+    public function __construct(int $id, string $adresse, string $client)
+    {
+        $this->id = $id;
+        $this->adresse = $adresse;
+        $this->client = $client;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdresse(): string
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param string $adresse
+     */
+    public function setAdresse(string $adresse): void
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClient(): string
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param string $client
+     */
+    public function setClient(string $client): void
+    {
+        $this->client = $client;
+    }
+
+
 
 }
