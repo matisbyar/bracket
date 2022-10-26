b0cd44604e75d76c729e41641a343bfefae47ea5
byarm <matis.byar@etu.umontpellier.fr>
2022-10-26 13:12:24 +0200

    Création de la base des classes Client et Produit

--- src/Model/DataObject/Client.php
+++ src/Model/DataObject/Client.php
@@ -3,5 +3,121 @@
 namespace App\Bracket\Model\DataObject;
 
 class Client extends AbstractDataObject {
+    private string $mail, $nom, $prenom, $dateNaissance, $adresse, $password;
+
+    /**
+     * @param string $mail
+     * @param string $nom
+     * @param string $prenom
+     * @param string $dateNaissance
+     * @param string $adresse
+     * @param string $password
+     */
+    public function __construct(string $mail, string $nom, string $prenom, string $dateNaissance, string $adresse, string $password)
+    {
+        $this->mail = $mail;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->dateNaissance = $dateNaissance;
+        $this->adresse = $adresse;
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMail(): string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param string $mail
+     */
+    public function setMail(string $mail): void
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateNaissance(): string
+    {
+        return $this->dateNaissance;
+    }
+
+    /**
+     * @param string $dateNaissance
+     */
+    public function setDateNaissance(string $dateNaissance): void
+    {
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdresse(): string
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param string $adresse
+     */
+    public function setAdresse(string $adresse): void
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
 
 }
--- src/Model/DataObject/Produit.php
+++ src/Model/DataObject/Produit.php
@@ -2,6 +2,142 @@
 
 namespace App\Bracket\Model\DataObject;
 
-class Produit {
+class Produit extends AbstractDataObject  {
+    private string $id, $type, $materiau, $couleur, $taille;
+    private float $prix;
+    private int $stock;
+
+    /**
+     * @param string $id
+     * @param string $type
+     * @param string $materiau
+     * @param string $couleur
+     * @param string $taille
+     * @param float $prix
+     * @param int $stock
+     */
+    public function __construct(string $id, string $type, string $materiau, string $couleur, string $taille, float $prix, int $stock)
+    {
+        $this->id = $id;
+        $this->type = $type;
+        $this->materiau = $materiau;
+        $this->couleur = $couleur;
+        $this->taille = $taille;
+        $this->prix = $prix;
+        $this->stock = $stock;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMateriau(): string
+    {
+        return $this->materiau;
+    }
+
+    /**
+     * @param string $materiau
+     */
+    public function setMateriau(string $materiau): void
+    {
+        $this->materiau = $materiau;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCouleur(): string
+    {
+        return $this->couleur;
+    }
+
+    /**
+     * @param string $couleur
+     */
+    public function setCouleur(string $couleur): void
+    {
+        $this->couleur = $couleur;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTaille(): string
+    {
+        return $this->taille;
+    }
+
+    /**
+     * @param string $taille
+     */
+    public function setTaille(string $taille): void
+    {
+        $this->taille = $taille;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrix(): float
+    {
+        return $this->prix;
+    }
+
+    /**
+     * @param float $prix
+     */
+    public function setPrix(float $prix): void
+    {
+        $this->prix = $prix;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStock(): int
+    {
+        return $this->stock;
+    }
+
+    /**
+     * @param int $stock
+     */
+    public function setStock(int $stock): void
+    {
+        $this->stock = $stock;
+    }
+
 
 }
