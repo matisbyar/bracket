4f804b677c01fa18da89210936455126cad3da2c
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-02 06:12:10 +0100

    Grille mise en place

--- src/view/produit/list.php
+++ src/view/produit/list.php
@@ -3,8 +3,12 @@
 echo '<div class="wrapper">';
 foreach ($produits as $produit) {
     $id = rawurlencode($produit->getId());
-    ?>
-    <?php echo '<div>' . "<a href=?action=read&id=" . $id . ">Produit " .
-        htmlspecialchars($produit->getId()) . " ".htmlspecialchars($produit->getNom())."</a></div>";
-    ?>
-<?php } echo "</div>" ?>
+    echo '<div class="produit">';
+    echo "<div class='haut'>Haut</div>";
+
+    echo '<div class="bas">' . "<a href=?action=read&id=" . $id . ">Produit " .
+        htmlspecialchars($produit->getId()) . " " . htmlspecialchars($produit->getNom()) . "</a></div>";
+
+    echo '</div>';
+}
+echo "</div>" ?>
