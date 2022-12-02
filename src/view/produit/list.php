256b4e1b51b8f66cd20c5cb706b7d64298344ee3
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-02 06:59:29 +0100

    CSS de l'affichage individuel d'un produit

--- src/view/produit/list.php
+++ src/view/produit/list.php
@@ -3,12 +3,19 @@
 echo '<div class="wrapper">';
 foreach ($produits as $produit) {
     $id = rawurlencode($produit->getId());
-    echo '<div class="produit">';
-    echo "<div class='haut'>Haut</div>";
+    echo '<a href="?action=read&id=' . $id .'" class="produit-a"><div class="produit">';
+    echo "<div class='haut'><img src='../../../images/" . $produit->getId() . ".png' alt='Produit'></div>";
 
-    echo '<div class="bas">' . "<a href=?action=read&id=" . $id . ">Produit " .
-        htmlspecialchars($produit->getId()) . " " . htmlspecialchars($produit->getNom()) . "</a></div>";
+    echo '<div class="bas">' . "
+            <p class='title-product-pane'>" . $produit->getNom() . "</p>
+            <p class='info-product-pane'>" . $produit->getMateriau() . "</p>
+            <div class='bottom-product-pane'>
+                <p class='info-product-pane'>" . $produit->getPrix() . "€</p>
+                <button id='acheter'>></button>
+            </div>
+            
+        </div>";
 
-    echo '</div>';
+    echo '</div></a>';
 }
 echo "</div>" ?>
