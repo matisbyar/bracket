abac7543416c875b74f8814d8b18c58457810863
Killian Reiner <killian.reiner@etu.umontpellier.fr>
2022-12-14 10:15:01 +0100

    resolution bug choix type bijou dans le formulaire

--- src/Controller/ControllerProduit.php
+++ src/Controller/ControllerProduit.php
@@ -4,23 +4,29 @@ namespace App\Bracket\Controller;
 
 use App\Bracket\Model\Repository\ProduitRepository;
 
-class ControllerProduit extends GenericController
+class ControllerProduit
 {
-    public static function error($errorMessage = ""): void
-    {
-        self::afficheVue("view.php", ["errorMessage" => $errorMessage, "pagetitle" => "Oups :/", "cheminVueBody" => "produit/error.php"]);
-    }
-
     public static function readAll(): void
     {
         $produits = (new ProduitRepository())->selectAll();
         self::afficheVue("view.php", ["produits" => $produits, "pagetitle" => "Liste des produits", "cheminVueBody" => "produit/list.php"]);
     }
 
+    private static function afficheVue(string $cheminVue, array $parametres = []): void
+    {
+        extract($parametres); // Crée des variables à partir du tableau $parametres
+        require __DIR__ . "/../view/$cheminVue"; // Charge la vue
+    }
+
     public static function read(): void
     {
         $produit = (new ProduitRepository())->select($_GET['id']);
         if ($produit != null) self::afficheVue("view.php", ["produit" => $produit, "pagetitle" => "Détail d'un produit", "cheminVueBody" => "produit/detail.php"]);
+    }
+
+    public static function error($errorMessage = ""): void
+    {
+        self::afficheVue("view.php", ["errorMessage" => $errorMessage, "pagetitle" => "Oups :/", "cheminVueBody" => "produit/error.php"]);
     }
 
     public static function home(): void
@@ -46,5 +52,13 @@ class ControllerProduit extends GenericController
             return $produit->getId() >= 200;
         });
         self::afficheVue("view.php", ["produits" => $produits, "pagetitle" => "Liste des bagues", "cheminVueBody" => "produit/list.php"]);
+    }
+
+    public static function create(): void{
+        ControllerProduit::afficheVue('view.php',["pagetitle"=>"Création d'un bijoux","cheminVueBody"=>"produit/create.php"]);
+    }
+
+    public static function created() : void{
+
     }
 }
