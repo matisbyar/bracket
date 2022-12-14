2e57aa498fe849a7394145221fe2d01449e5c51c
Killian Reiner <killian.reiner@etu.umontpellier.fr>
2022-12-14 09:39:39 +0100

    Création du formulaire pour créer un bijou

---
+++ src/view/produit/create.php
@@ -0,0 +1,42 @@
+<form action="../web/frontController.php">
+    <fieldset>
+        <legend>Création d'un bijou</legend>
+        <input type='hidden' name='action' value='created'>
+        <fieldset>
+            <legend>Choix du type</legend>
+            <div>
+                <input type="radio" id="bagueId" name="bague" value="bague" checked>
+                <label for="bagueId">Bague</label>
+            </div>
+            <div>
+                <input type="radio" id="braceletId" name="bracelet" value="bracelet">
+                <label for="braceletId">Bracelet</label>
+            </div>
+        </fieldset>
+        <p>
+            <label for="prixId">Prix</label> :
+            <input type="number" placeholder="150" name="prix" id="prixId" required/>
+        </p>
+        <p>
+            <label for="materiauId">Matériau</label> :
+            <select name="materiau" size="1" id="materiauId">
+                <option>Acier Inoxydable</option>
+                <option>Argent</option>
+                <option>Or</option>
+                <option>Plaque Or</option>
+                <option>Tissus</option>
+            </select>
+        </p>
+        <p>
+            <label for="nomId">Nom</label> :
+            <input type="text" placeholder="Bracelet Georges" name="nom" id="nomId" required/>
+        </p>
+        <p>
+            <label for="descriptionId">Description</label> :
+            <input type="text" placeholder="" name="description" id="descriptionId" required/>
+        </p>
+        <p>
+            <input type="submit" value="Envoyer" />
+        </p>
+    </fieldset>
+</form>
