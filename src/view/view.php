4040b74af451a845432a60bca61b517b90800ade
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-14 09:10:24 +0100

    login + logout + account

--- src/view/view.php
+++ src/view/view.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $pagetitle; ?></title>
-<link rel="stylesheet" href="../css/styles.css">
+    <title><?php use App\Bracket\Lib\ConnexionUtilisateur;
+
+        echo $pagetitle; ?></title>
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
 <header>
@@ -12,6 +14,18 @@
         <div class="nav-raccourcis">
             <a href="../web/frontController.php?action=readAllBracelets"><p>Bracelets</p></a>
             <a href="../web/frontController.php?action=readAllBagues"><p>Bagues</p></a>
+            <?php if (ConnexionUtilisateur::estConnecte()) { ?>
+                <li><a href="../web/frontController.php?action=account">
+                        <button>account<img src="../../images/person.svg" alt="Fav" style="width:30px;height:30px;"></button>
+                    </a></li>
+                <li><a href="../web/frontController.php?action=logout">
+                        <button>logout<img src="../../images/person.svg" alt="Fav" style="width:30px;height:30px;"></button>
+                    </a></li>
+            <?php } else { ?>
+                <li><a href="../web/frontController.php?action=login">
+                        <button>login<img src="../../images/person.svg" alt="Fav" style="width:30px;height:30px;"></button>
+                    </a></li>
+            <?php } ?>
         </div>
     </nav>
 </header>
