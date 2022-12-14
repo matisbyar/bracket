adc3e3efe17fda6e1b9a9cee3c3562bbfe14c918
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-14 09:59:25 +0100

    login, account

--- src/Controller/ControllerClient.php
+++ src/Controller/ControllerClient.php
@@ -2,6 +2,9 @@
 
 namespace App\Bracket\Controller;
 
+use App\Bracket\Lib\ConnexionUtilisateur;
+use App\Bracket\Lib\MessageFlash;
+use App\Bracket\Lib\MotDePasse;
 use App\Bracket\Model\Repository\ClientRepository;
 
 class ControllerClient
@@ -28,5 +31,20 @@ class ControllerClient
     {
         $client = (new ClientRepository())->select($_GET['email']);
         if ($client != null) self::afficheVue("view.php", ["client" => $client, "pagetitle" => "Modifier un client", "cheminVueBody" => "client/update.php"]);
+    }
+
+    public static function connecter()
+    {
+        $utilisateur = (new ClientRepository())->select($_POST['login']);
+        if ($utilisateur == null) {
+            MessageFlash::ajouter("Danger","L'identifiant est incorrect");
+        } else {
+            if ($_POST['motdepasse'] == MotDePasse::verifier($_POST['motdepasse'], $utilisateur->getMdpHache())) {
+                ConnexionUtilisateur::connecter($utilisateur->getMail());
+                header('Location: frontController.php?controller=utilisateur&action=read&login=' . rawurlencode($utilisateur->getMail()));
+            } else {
+                MessageFlash::ajouter("Danger","Le mot de passe est incorrect");
+            }
+        }
     }
 }
--- src/Model/Repository/ClientRepository.php
+++ src/Model/Repository/ClientRepository.php
@@ -16,4 +16,6 @@ class ClientRepository extends AbstractRepository {
         return "email";
     }
 
+
+
 }
