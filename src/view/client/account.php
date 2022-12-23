5d40f19b955b4e63353b8d75fcc4e6adebf37a1f
Killian Reiner <killian.reiner@etu.umontpellier.fr>
2022-12-23 15:24:48 +0100

    Avancée de la vue du compte

--- src/view/client/account.php
+++ src/view/client/account.php
@@ -1,1 +1,13 @@
 <?php
+use App\Bracket\Lib\ConnexionUtilisateur;
+use App\Bracket\Model\Repository\ClientRepository;
+$client = (new ClientRepository)->read(ConnexionUtilisateur::getLoginUtilisateurConnecte());
+echo "<p><strong>Identifiant : </strong>".htmlspecialchars($client->getMail())."</p>";
+echo "<p><strong>Nom : </strong>".htmlspecialchars($client->getNom())."</p>";
+echo "<p><strong>Prénom : </strong>".htmlspecialchars($client->getPrenom())."</p>";
+echo "<p><strong>Date de naissance : </strong>".htmlspecialchars($client->getDateNaissance())."</p>";
+echo "<p><strong>Adresse : </strong>".htmlspecialchars($client->getAdresse())."</p>";
+echo "<p><strong>Description : </strong>".htmlspecialchars($client->getDescription())."</p>";
+
+echo "<p><a href=\"?action=update&controller=client&email=".rawurlencode(ConnexionUtilisateur::getLoginUtilisateurConnecte())."\">Modification des informations du compte</p>";
+echo "<p><a href=\"?action=updatePassword&controller=client&email=".rawurlencode(ConnexionUtilisateur::getLoginUtilisateurConnecte())."\">Modification du mot de passe</p>";
