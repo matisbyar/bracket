568b5da090ed538d590036739207883d1855c918
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-24 11:58:39 +0100

    styling détail

--- src/view/produit/detail.php
+++ src/view/produit/detail.php
@@ -1,9 +1,20 @@
 <?php
 /** @var Produit $produit */
-echo '<h1>' . ucfirst($produit->getType()) . '</h1>';
-echo '<img src="../../images/' . $produit->getId() . '.png" alt="Image du bracelet">';
-echo '<p>' . $produit->getNom() . ' — '
-    . $produit->getPrix() . ' € — '
-    . $produit->getMateriau() .
-    ' — ' . $produit->getDescription() . '</p>';
+
+
+echo '<h1 class="h1Description">' . ucfirst($produit->getType()) . '</h1>';
+
+/* Image */
+echo '<section class="imageAfficheDescription">';
+echo '<img class="imageDescription" src="../../images/' . $produit->getId() . '.png" alt="Image du bracelet">';
+echo '</section>';
+
+/* Description */
+
+echo '<section class="description">';
+echo '<div>'. '- ' . $produit->getNom() . '</div>';
+echo '<div>'. '- ' . $produit->getMateriau() . '</div>';
+echo '<div>'. '- ' . $produit->getDescription() . '</div>';
+echo '<div class="lastDesc">' . $produit->getPrix() . ' € </div>';
+echo '</section>';
 ?>
