c3fb0bdc3e61dcebc224337018681eba305b04d4
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-24 11:16:08 +0100

    styling account

--- src/view/client/account.php
+++ src/view/client/account.php
@@ -2,13 +2,16 @@
 use App\Bracket\Lib\ConnexionUtilisateur;
 use App\Bracket\Model\Repository\ClientRepository;
 $client = (new ClientRepository)->read(ConnexionUtilisateur::getLoginUtilisateurConnecte());
-echo "<p><strong>Identifiant : </strong>".htmlspecialchars($client->getMail())."</p>";
-echo "<p><strong>Nom : </strong>".htmlspecialchars($client->getNom())."</p>";
-echo "<p><strong>Prénom : </strong>".htmlspecialchars($client->getPrenom())."</p>";
-echo "<p><strong>Date de naissance : </strong>".htmlspecialchars($client->getDateNaissance())."</p>";
-echo "<p><strong>Adresse : </strong>".htmlspecialchars($client->getAdresse())."</p>";
-echo "<p><strong>Description : </strong>".htmlspecialchars($client->getDescription())."</p>";
-
-echo "<p><a href=\"?action=update&controller=client&email=".rawurldecode(ConnexionUtilisateur::getLoginUtilisateurConnecte())."\">Modification des informations du compte</p>";
-echo "<p><a href=\"?action=updatePassword&controller=client\">Modification du mot de passe</p>";
-echo "<p><a href=\"?controller=client&action=logout\">Se déconnecter</p>";
+echo "<section class='subInfoCompte'>";
+echo "<div class='infoCompte'><p><strong>Identifiant :</strong> ".htmlspecialchars($client->getMail())."</p></div>";
+echo "<div class='infoCompte'><p><strong>Nom :</strong> ".htmlspecialchars($client->getNom())."</p></div>";
+echo "<div class='infoCompte'><p><strong>Prénom :</strong> ".htmlspecialchars($client->getPrenom())."</p></div>";
+echo "<div class='infoCompte'><p><strong>Date de naissance :</strong> ".htmlspecialchars($client->getDateNaissance())."</p></div>";
+echo "<div class='infoCompte'><p><strong>Adresse :</strong> ".htmlspecialchars($client->getAdresse())."</p></div>";
+echo "<div class='infoCompte'><p><strong>Description :</strong> ".htmlspecialchars($client->getDescription())."</p></div>";
+echo "</section>";
+echo "<section>";
+echo "<div class='infoCompteButton'><p><button class='buttonOnForm'><a href=\"?action=update&controller=client&email=".rawurldecode(ConnexionUtilisateur::getLoginUtilisateurConnecte())."\">Modification des informations du compte</button></p></div>";
+echo "<div class='infoCompteButton'><p><button class='buttonOnForm'><a href=\"?action=updatePassword&controller=client\">Modification du mot de passe</button></p></div>";
+echo "<div class='infoCompteButton'><p><button class='buttonOnForm'><a href=\"?controller=client&action=logout\">Se déconnecter</button></p></div>";
+echo "</section>";
