5a86b9fc2260246e74011441df7b930b58b9dfb8
Killian Reiner <killian.reiner@etu.umontpellier.fr>
2022-12-24 12:54:01 +0100

    resolution bug image bijou read

--- src/view/produit/detail.php
+++ src/view/produit/detail.php
@@ -11,7 +11,7 @@ echo '<h1 class="h1Description">' . ucfirst($produit->getType()) . '</h1>';
 
 /* Image */
 echo '<section class="imageAfficheDescription">';
-echo '<img class="imageDescription" src="../../images/' . $produit->getId() . '.png" alt="Image du bracelet">';
+echo '<img class="imageDescription" src="../images/' . $produit->getId() . '.png" alt="Image du bracelet">';
 echo '</section>';
 
 /* Description */
