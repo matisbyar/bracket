d54a1512f98c2505409b0eba1d8eaa6c875fce92
matisbyar <matis.byar.06@icloud.com>
2022-12-28 11:41:49 +0100

    Refactoring

--- src/view/client/account.php
+++ src/view/client/account.php
@@ -1,21 +1,17 @@
 <?php
+
 use App\Bracket\Lib\ConnexionUtilisateur;
 use App\Bracket\Model\Repository\ClientRepository;
 
-?>
-    <div class="back-btn">
-        <button onclick="history.go(-1);"><i class="fas fa-arrow-left"><img src="../images/backPage.svg"
-                                                                            alt="button retour"></i></button>
-    </div>
-<?php
+echo '<div class="back-btn"><button onclick="history.go(-1);"><i class="fas fa-arrow-left"><img src="../images/backPage.svg" alt="button retour"></i></button></div>';
 
 $client = (new ClientRepository)->read(ConnexionUtilisateur::getLoginUtilisateurConnecte());
 echo "<section class='subInfoCompte'>";
-echo "<div class='infoCompte'><p><strong>Adresse e-mail :</strong> ".htmlspecialchars($client->getMail())."</p></div>";
-echo "<div class='infoCompte'><p><strong>Nom :</strong> ".htmlspecialchars($client->getNom())."</p></div>";
-echo "<div class='infoCompte'><p><strong>Prénom :</strong> ".htmlspecialchars($client->getPrenom())."</p></div>";
-echo "<div class='infoCompte'><p><strong>Date de naissance :</strong> ".htmlspecialchars($client->getDateNaissance())."</p></div>";
-echo "<div class='infoCompte'><p><strong>Adresse :</strong> ".htmlspecialchars($client->getAdresse())."</p></div>";
+echo "<div class='infoCompte'><p><strong>Adresse e-mail :</strong> " . htmlspecialchars($client->getMail()) . "</p></div>";
+echo "<div class='infoCompte'><p><strong>Nom :</strong> " . htmlspecialchars($client->getNom()) . "</p></div>";
+echo "<div class='infoCompte'><p><strong>Prénom :</strong> " . htmlspecialchars($client->getPrenom()) . "</p></div>";
+echo "<div class='infoCompte'><p><strong>Date de naissance :</strong> " . htmlspecialchars($client->getDateNaissance()) . "</p></div>";
+echo "<div class='infoCompte'><p><strong>Adresse :</strong> " . htmlspecialchars($client->getAdresse()) . "</p></div>";
 if ($client->estAdmin()) {
     echo "<div class='infoCompte' id='admin'><p><strong>Administrateur : </strong>Oui</p></div>";
 }
@@ -25,7 +21,7 @@ echo "<section>";
 if ($client->estAdmin()) {
     echo "<div class='infoCompteButton' ><p><button class='buttonOnForm' id='admin'><a href=\"?action=readAll&controller=client\">Accéder au panel admin</button></p></div>";
 }
-echo "<div class='infoCompteButton'><p><button class='buttonOnForm'><a href=\"?action=update&controller=client&email=".rawurldecode(ConnexionUtilisateur::getLoginUtilisateurConnecte())."\">Modification des informations du compte</button></p></div>";
+echo "<div class='infoCompteButton'><p><button class='buttonOnForm'><a href=\"?action=update&controller=client&email=" . rawurldecode(ConnexionUtilisateur::getLoginUtilisateurConnecte()) . "\">Modification des informations du compte</button></p></div>";
 echo "<div class='infoCompteButton'><p><button class='buttonOnForm'><a href=\"?action=updatePassword&controller=client\">Modification du mot de passe</button></p></div>";
 echo "<div class='infoCompteButton'><p><button class='buttonOnForm'><a href=\"?controller=client&action=logout\">Se déconnecter</button></p></div>";
 echo "</section>";
