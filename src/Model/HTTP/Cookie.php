2944ad96d4b5b1cd7e1ce5682669cd0e35d6e690
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-30 19:36:35 +0100

    Add commentary

--- src/Model/HTTP/Cookie.php
+++ src/Model/HTTP/Cookie.php
@@ -3,19 +3,41 @@ namespace App\Bracket\Model\HTTP;
 
 class Cookie {
 
+    /**
+     * Crée un cookie
+     * @param string $cle
+     * @param mixed $valeur
+     * @param int|null $dureeExpiration
+     * @return void
+     */
     public static function enregistrer(string $cle, mixed $valeur, ?int $dureeExpiration = null): void {
         if ($dureeExpiration == null) setCookie($cle, serialize($valeur), 0);
         else setCookie($cle, serialize($valeur), time() + $dureeExpiration);
     }
 
+    /**
+     * Récupère un cookie
+     * @param string $cle
+     * @return mixed
+     */
     public static function lire(string $cle) : mixed {
         return self::contient($cle) ? unserialize($_COOKIE[$cle]) : "";
     }
 
+    /**
+     * Contient le cookie
+     * @param $cle
+     * @return bool
+     */
     public static function contient($cle) : bool {
         return isset($_COOKIE[$cle]);
     }
 
+    /**
+     * Supprime un cookie
+     * @param $cle
+     * @return void
+     */
     public static function supprimer($cle) : void {
         unset($_COOKIE[$cle]);
         setcookie($cle, "", 1);
