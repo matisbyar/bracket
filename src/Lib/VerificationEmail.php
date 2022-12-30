92905a0553eeae7b8fc0d1b53a297ac7f05229aa
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-30 19:29:41 +0100

    Add commentary

--- src/Lib/VerificationEmail.php
+++ src/Lib/VerificationEmail.php
@@ -9,6 +9,11 @@ use App\Bracket\Model\Repository\ClientRepository;
 class VerificationEmail
 {
 
+    /**
+     * Envoie le mail entré pour validation
+     * @param Client $client
+     * @return void
+     */
     public static function envoiEmailValidation(Client $client): void
     {
         $loginURL = rawurlencode($client->getMail());
@@ -22,6 +27,12 @@ class VerificationEmail
         // mail($utilisateur->getEmail(), "Validation de votre email", "Votre email a été validé.");
     }
 
+    /**
+     * Traite la validation de l'email
+     * @param string $login
+     * @param string $nonce
+     * @return bool
+     */
     public static function traiterEmailValidation($login, $nonce): bool
     {
         $client = (new ClientRepository())->select($login);
@@ -37,6 +48,7 @@ class VerificationEmail
     }
 
     /**
+     * Retourne si l'email est validé
      * @param Client $client
      * @return bool vrai si le mail est à valider. Faux sinon.
      */
