6808cf5950ded1682e6c82f7e2896a31941c8c28
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-30 19:44:34 +0100

    Add commentary

--- src/Model/Repository/CommandeRepository.php
+++ src/Model/Repository/CommandeRepository.php
@@ -4,18 +4,36 @@ namespace App\Bracket\Model\Repository;
 use App\Bracket\Model\DataObject\Commande;
 
 class CommandeRepository extends AbstractRepository {
+
+    /**
+     * Constructeur
+     * @param array $commandeFormatTableau
+     * @return Commande
+     */
     public function construire (array $commandeFormatTableau): Commande {
         return new Commande($commandeFormatTableau['0'], $commandeFormatTableau['1'], $commandeFormatTableau['2'], $commandeFormatTableau['3']);
     }
 
+    /**
+     * Retourne le nom de la table
+     * @return string
+     */
     protected function getNomTable(): string {
         return "p_commandes";
     }
 
+    /**
+     * Retourne le nom de la cle primaire
+     * @return string
+     */
     protected function getNomClePrimaire(): string {
         return "id";
     }
 
+    /**
+     * Retourne le nom de la colonne
+     * @return string[]
+     */
     protected function getNomColonnes(): array
     {
         return array(
