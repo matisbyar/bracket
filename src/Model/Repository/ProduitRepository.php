c14a9900853e91d9e9d8c2ec07474e448085e34f
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-30 19:45:56 +0100

    Add commentary

--- src/Model/Repository/ProduitRepository.php
+++ src/Model/Repository/ProduitRepository.php
@@ -7,21 +7,38 @@ use PDOException;
 
 class ProduitRepository extends AbstractRepository
 {
+    /**
+     * Constructeur
+     * @param array $produitFormatTableau
+     * @return Produit
+     */
     public function construire(array $produitFormatTableau): Produit
     {
         return new Produit($produitFormatTableau['0'], $produitFormatTableau['1'], $produitFormatTableau['2'], $produitFormatTableau['3'], $produitFormatTableau['4'], $produitFormatTableau['5'], $produitFormatTableau['6']);
     }
 
+    /**
+     * Retourne le nom de la table
+     * @return string
+     */
     protected function getNomTable(): string
     {
         return "p_bijoux";
     }
 
+    /**
+     * Retourne le nom de la cle primaire
+     * @return string
+     */
     protected function getNomClePrimaire(): string
     {
         return "id";
     }
 
+    /**
+     * Retourne le nom de la colonne
+     * @return string[]
+     */
     protected function getNomColonnes(): array
     {
         return array(
@@ -29,6 +46,11 @@ class ProduitRepository extends AbstractRepository
         );
     }
 
+    /**
+     * Retourne le produit par son id
+     * @param $typeBijou
+     * @return int
+     */
     public function getId ($typeBijou) : int
     {
         try {
