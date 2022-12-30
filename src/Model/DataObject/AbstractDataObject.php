2ec102290c1b8a03357263d7a3c11eb33e7536ab
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-30 19:32:06 +0100

    Add commentary

--- src/Model/DataObject/AbstractDataObject.php
+++ src/Model/DataObject/AbstractDataObject.php
@@ -5,6 +5,10 @@ namespace App\Bracket\Model\DataObject;
 abstract class AbstractDataObject
 {
 
+    /**
+     * Transforme l'objet en tableau associatif
+     * @return array
+     */
     public abstract function formatTableau(): array;
 
 }
