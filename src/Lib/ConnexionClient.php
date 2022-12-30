dc737ce2a5236f6ce21fa2b27c4c1846431cfd44
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-30 19:21:37 +0100

    Add commentary

--- src/Lib/ConnexionClient.php
+++ src/Lib/ConnexionClient.php
@@ -10,6 +10,11 @@ class ConnexionClient
     // L'utilisateur connecté sera enregistré en session associé à la clé suivante
     private static string $cleConnexion = "_utilisateurConnecte";
 
+    /**
+     * Connexion d'un utilisateur
+     * @param string $loginUtilisateur
+     * @return void
+     */
     public static function connecter(string $loginUtilisateur): void
     {
         // À compléter
@@ -17,26 +22,46 @@ class ConnexionClient
         var_dump(Session::getInstance()->lire(self::$cleConnexion));
     }
 
+    /**
+     * Retourne si l'utilisateur est connecté
+     * @return bool
+     */
     public static function estConnecte(): bool
     {
         return Session::getInstance()->contient(self::$cleConnexion);
     }
 
+    /**
+     * Decoonection d'un utilisateur
+     * @return void
+     */
     public static function deconnecter(): void
     {
         Session::getInstance()->supprimer(self::$cleConnexion);
     }
 
+    /**
+     * Retourne le login de l'utilisateur connecté
+     * @return ?string
+     */
     public static function getLoginUtilisateurConnecte(): ?string
     {
         return Session::getInstance()->lire(self::$cleConnexion);
     }
 
+    /**
+     * Retourne si c'est bien l'utilisateur connecté
+     * @return bool
+     */
     public static function estUtilisateur($login): bool
     {
         return self::getLoginUtilisateurConnecte() === $login;
     }
 
+    /**
+     * Retourne si l'utilisateur connecté est un administrateur
+     * @return bool
+     */
     public static function estAdministrateur(): bool
     {
         return self::estConnecte() && (new ClientRepository())->select(self::getLoginUtilisateurConnecte())->estAdmin();
