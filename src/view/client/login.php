8c1972ee1c6c65ac6cb067851b2cc1dca0905daa
Killian Reiner <killian.reiner@etu.umontpellier.fr>
2023-01-02 21:15:55 +0100

    Modif du champ name dans le formulaire

--- src/view/client/login.php
+++ src/view/client/login.php
@@ -9,12 +9,12 @@
             <input type='hidden' name='action' value='created'>
             <p class="InputAddOn">
                 <label class="InputAddOn-item" for="nomId">Nom&#42;</label>
-                <input class="InputAddOn-field" type="text" placeholder="nom" name="Nom" id="nomId"
+                <input class="InputAddOn-field" type="text" placeholder="nom" name="nom" id="nomId"
                        required/>
             </p>
             <p class="InputAddOn">
                 <label class="InputAddOn-item" for="prenomId">Prénom&#42;</label>
-                <input class="InputAddOn-field" type="text" placeholder="prenom" name="Prénom" id="nomId" required/>
+                <input class="InputAddOn-field" type="text" placeholder="prenom" name="prenom" id="nomId" required/>
             </p>
             <p class="InputAddOn">
                 <label class="InputAddOn-item" for="naissanceId">Date de naissance&#42;</label>
