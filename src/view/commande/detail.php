f1d96242f78061896c7a7d212d09e569e9344965
Killian Reiner <killian.reiner@etu.umontpellier.fr>
2023-01-05 08:36:13 +0100

    Modif de la vue détail pour les admins

--- src/view/commande/detail.php
+++ src/view/commande/detail.php
@@ -1,14 +1,25 @@
-<?<?php
-  use App\Bracket\Model\Repository\ProduitRepository;
+<?php
+
+use App\Bracket\Lib\ConnexionClient;
+use App\Bracket\Model\DataObject\Commande;
+use App\Bracket\Model\Repository\ProduitRepository;
 /** @var Commande $commande */
 echo "<section>";
 echo "<h2>Commande n°" . htmlspecialchars($commande->getId()) . "</h2>";
 echo "Livré à : " . htmlspecialchars($commande->getAdresse()) . "<br>";
+if (ConnexionClient::estAdministrateur()) {
+    echo "Commande passé par : " . htmlspecialchars($commande->getClient()) . "<br>";
+    echo "Statut : " . htmlspecialchars($commande->getStatut()) . "<br>";
+}
 echo "Liste des produits : ";
 echo "<ul>";
 foreach ($commande->getArticles() as $article) {
     $bijou = (new ProduitRepository)->select($article->getIdBijou());
-    echo "<li>" . htmlspecialchars($bijou->getNom()) . " : " . htmlspecialchars($bijou->getPrix()) . "€</li>";
+    if (ConnexionClient::estAdministrateur()){
+        echo "<li>" . htmlspecialchars($bijou->getNom()) . " : " . htmlspecialchars($bijou->getPrix()) . "€" .
+            "<a href='index.php?controller=commande&action=update&id=" . htmlspecialchars($commande->getId()) . "'> Changer le statut </a>".
+            "<a href='index.php?controller=commande&action=delete&id=" . htmlspecialchars($commande->getId()) . "'>Supprimer</a></li>";
+    }
 }
 echo "</ul>";
 
--- src/view/commande/list.php
+++ src/view/commande/list.php
@@ -1,5 +1,8 @@
 <?php
 /** @var Commande $commandes */
+
+use App\Bracket\Model\DataObject\Commande;
+
 foreach ($commandes as $commande){
     echo "<p>Commande n°" . htmlspecialchars($commande->getId()) . " : <a href='index.php?controller=commande&action=read&id=" . htmlspecialchars($commande->getId()) . "'>Détail</a></p>";
 }
