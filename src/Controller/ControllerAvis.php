d0fbc2e30bc8b5775185b45e79f222ddc7329258
matisbyar <matis.byar.06@icloud.com>
2023-01-05 13:28:41 +0100

    Protection de l'ajout d'un avis @vanhovev @reinerk

--- src/Controller/ControllerAvis.php
+++ src/Controller/ControllerAvis.php
@@ -12,11 +12,16 @@ class ControllerAvis extends GenericController
     public static function ajouterAvis(): void
     {
         if (ConnexionClient::estConnecte()) {
-            $tableauFormulaire = $_REQUEST;
-            $avis = Avis::construireDepuisFormulaire($tableauFormulaire);
-            (new AvisRepository())->create($avis);
-            MessageFlash::ajouter("success", "Votre avis a bien été ajouté, merci !");
-            self::redirige("?controller=produit&action=read&id=" . $avis->getIdBijou());
+            if (isset($_REQUEST['idBijou']) && isset($_REQUEST['mailClient']) && isset($_REQUEST['note']) && isset($_REQUEST['avis'])) {
+                $tableauFormulaire = $_REQUEST;
+                $avis = Avis::construireDepuisFormulaire($tableauFormulaire);
+                (new AvisRepository())->create($avis);
+                MessageFlash::ajouter("success", "Votre avis a bien été ajouté, merci !");
+                self::redirige("?controller=produit&action=read&id=" . $avis->getIdBijou());
+            } else {
+                MessageFlash::ajouter("danger", "Une erreur est survenue lors de l'ajout d'un avis, veuillez réessayer.");
+                self::home();
+            }
         } else {
             MessageFlash::ajouter("danger", "Vous devez être connecté pour ajouter un avis !");
             self::redirige("?controller=produit&action=readAll");
